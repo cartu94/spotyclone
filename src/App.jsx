cfef1975f6395d4d6d5d36a1efa5a471c612ecd4
andreabianco1996 <andreabinky@gmail.com>
2023-12-15 14:31:55 +0100

    Navbar + LibraryNav + SearchHome

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 // import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import LibraryNav from "./components/LibraryNav";
 import Navbar from "./components/Navbar";
+import SearchHome from "./components/SearchHome";
 
 export default function App() {
   return (
     <div>
       <Navbar />
       <LibraryNav />
+      <SearchHome />
     </div>
   );
 }
