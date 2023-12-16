c85a6d2b5930af3b57316754f86fb20d9169c42e
andreabianco1996 <andreabinky@gmail.com>
2023-12-16 19:08:44 +0100

    Altre Modifiche

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,49 +2,6 @@ import Icons from "../components/Icons";
 
 export default function Footer() {
   return (
-    // <footer className="h-20 text-white p-5 absolute bottom-0 left-0 w-full font-semibold md:flex md:items-center md:justify-between">
-    //   <div className="mx-auto flex flex-col gap-4 md:flex-row md:justify-between md:items-center items-center justify-between">
-    //     <div className="flex items-center justify-between">
-    //       <img
-    //         src="src\assets\img\spotyclone.png"
-    //         alt="Spotyclone Logo"
-    //         className="h-7"
-    //       />
-    //       <p className="text-xs">
-    //         <span className="text-hover-details font-bold text-lg px-2">
-    //           Spotyclone
-    //         </span>{" "}
-    //         © 2023
-    //       </p>
-    //     </div>
-    //     <div className="flex items-center space-x-8">
-    //       <a
-    //         href="#"
-    //         className="text-sm hover:text-gray-300 transition duration-300"
-    //       >
-    //         Legal
-    //       </a>
-    //       <a
-    //         href="#"
-    //         className="text-sm hover:text-gray-300 transition duration-300"
-    //       >
-    //         Privacy Center
-    //       </a>
-    //       <a
-    //         href="#"
-    //         className="text-sm hover:text-gray-300 transition duration-300"
-    //       >
-    //         Privacy Policy
-    //       </a>
-    //       <a
-    //         href="#"
-    //         className="text-sm hover:text-gray-300 transition duration-300"
-    //       >
-    //         Cookies
-    //       </a>
-    //     </div>
-    //   </div>
-    // </footer>
     <footer className="text-inactive font-semibold px-5 py-6 absolute bottom-0 left-0 w-full">
       <div className="flex items-center justify-between m-auto">
         <button className="flex items-center justify-between gap-2">
--- src/components/LibraryCaseRow.jsx
+++ src/components/LibraryCaseRow.jsx
@@ -2,7 +2,7 @@ export default function LibraryCaseRow() {
   return (
     <div className="bg-background w-full lg:w-full flex justify-center lg:justify-start items-center px-0 py-2 lg:px-4 lg:py-3 gap-4 rounded-xl group transition duration-300 cursor-pointer hover:bg-white/20">
       <img
-        className="h-12 w-12 rounded-xl lg:h-16 lg:w-16"
+        className="h-12 w-12 rounded-lg lg:h-16 lg:w-16"
         src="https://picsum.photos/100"
         alt=""
       />
