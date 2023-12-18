0add4978d207c685aaf87eef540f363cf4acf874
yocopk <yocopk@gmail.com>
2023-12-18 11:40:56 +0100

    HomeGreetings titles update

--- src/components/home_greetings/HomeGreetings.jsx
+++ src/components/home_greetings/HomeGreetings.jsx
@@ -2,41 +2,48 @@ import HomeGreetingsCard from "./HomeGreetingsCard";
 import { useEffect, useState } from "react";
 
 export default function HomeGreetings() {
-  const [greetings, setGreetings] = useState("Buongiorno");
-  
+  const [greetings, setGreetings] = useState("Buongiorno"); // stringa "Buongiorno" con useState, in modo che possa ascoltare i cambiamenti
+  const [greetingsData, setGreetingsData] = useState([]); // inizializzo un array vuoto
+
+  // Recuperiamo il json in locale
+  useEffect(() => {
+    fetch("data/homegreetings.json")
+      .then((res) => res.json())
+      .then((data) => {
+        setGreetingsData(data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, []);
+
+  // Diamo alla stringa "Buongiorno" una dinamicità in base all'orario reale.
   useEffect(() => {
     const interval = setInterval(() => {
       const date = new Date().getHours();
       console.log("ore:", date);
-      if(date > 6 && date <= 12) {
+      if (date > 6 && date <= 12) {
         setGreetings("Buongiorno");
-      } else if(date > 12 && date <= 18) {
+      } else if (date > 12 && date <= 18) {
         setGreetings("Buon pomeriggio");
-      }
-      else if(date > 18 && date <= 22) {
+      } else if (date > 18 && date <= 22) {
         setGreetings("Buona sera");
-      }
-      else {
+      } else {
         setGreetings("Buona notte");
       }
     }, 60000);
 
     return () => clearTimeout(interval);
-
   }, []);
 
   return (
     <div className="bg-gradient-to-b from-cyan-950 to-background text-active w-full flex flex-col px-4 py-2">
-      <h1 className="text-4xl mb-8 font-bold">
-        {greetings}
-      </h1>
+      <h1 className="text-4xl mb-8 font-bold">{greetings}</h1>
       <div className="grid gap-2 grid-cols-my-2 md:grid-cols-my-3">
-        <HomeGreetingsCard/>
-        <HomeGreetingsCard/>
-        <HomeGreetingsCard/>
-        <HomeGreetingsCard/>
-        <HomeGreetingsCard/>
-        <HomeGreetingsCard/>
+        {/* Iteriamo il titolo in modo che prenda ogni volta i dati del titolo direttamente dal json */}
+        {greetingsData.map((greeting) => (
+          <HomeGreetingsCard key={greeting.id} title={greeting.title} />
+        ))}
       </div>
     </div>
   );
--- src/components/home_greetings/HomeGreetingsCard.jsx
+++ src/components/home_greetings/HomeGreetingsCard.jsx
@@ -1,10 +1,18 @@
+/* eslint-disable react/prop-types */
 import PlayButton from "../PlayButton";
-export default function HomeGreetingsCard() {
+export default function HomeGreetingsCard({ title }) {
   return (
     <div className="flex w-full h-[60px] min-w-[220px] bg-white/10 rounded-lg group transition-all duration-300 cursor-pointer hover:bg-white/20 relative">
-      <img className="rounded-l-lg" src={`https://picsum.photos/seed/${Math.floor(Math.random() * 1000)}/100`} alt="greetings img" />
+      <img
+        className="rounded-l-lg"
+        src={`https://picsum.photos/seed/${Math.floor(
+          Math.random() * 1000
+        )}/100`}
+        alt="greetings img"
+      />
       <div className="flex items-center justify-between m-auto px-2 w-full">
-        <p className="text-active text-sm font-semibold">Brani che ti piacciono</p>
+        {/* Andiamo a richiamare il titolo attraverso la prop in alto */}
+        <p className="text-active text-sm font-semibold">{title}</p>
         <div className="w-10 h-10 opacity-0 group-hover:opacity-100 transition-all duration-300 absolute right-4">
           <PlayButton />
         </div>
@@ -12,4 +20,3 @@ export default function HomeGreetingsCard() {
     </div>
   );
 }
-          
